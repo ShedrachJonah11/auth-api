0f8053c6a6861dde3f1b71b2346c4b6d962769e8
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2025-01-31 11:06:50 +0100

    refactor: optimize middleware chain

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,8 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    // Logger initialized}
 
   @Get('status')
   getStatus(): object {
