3a2f4eec0f7d407ec1ba6b1159dca616a7a3b44c
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2025-02-09 05:12:54 +0100

    feat: create password reset functionality

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    // Optimized at 1762482316885
   const app = await NestFactory.create(AppModule);
   
   // Enable CORS
