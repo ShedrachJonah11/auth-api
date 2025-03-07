8c9a4bace983d0e1467dd9e048305955412f4f02
tommy-skywalker  <babatomiwasokeyo@gmail.com>
2025-03-07 12:27:00 +0100

    perf: optimize database queries

---
+++ src/common/dto/update-feature46.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateFeature46Dto } from './create-feature46.dto';
+
+export class UpdateFeature46Dto extends PartialType(CreateFeature46Dto) {}
