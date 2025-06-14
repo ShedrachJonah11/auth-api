d1c57f93279f6800c58702eb9fe2486910a651f2
Shedrach Jonah <shedrachjonah11@gmail.com>
2025-06-14 04:18:49 +0100

    feat: changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/user-auth-api'),
+    MongooseModule.forRoot('mongodb://localhost:27017/user-auth'),
   ],
   controllers: [AppController],
   providers: [AppService],
