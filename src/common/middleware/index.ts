be37d9bbb518c9b606e7afd6cebc65404b4cb789
OLU-DEVX <shedrachjonah22@gmail.com>
2026-04-08 21:27:03 +0100

    refactor(common): add middleware barrel export

---
+++ src/common/middleware/index.ts
@@ -0,0 +1,5 @@
+export * from './api-version.middleware';
+export * from './correlation-id.middleware';
+export * from './logging.middleware';
+export * from './request-id.middleware';
+export * from './security.middleware';
